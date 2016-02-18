6672cc5a1a6541dd16f5a36663b189512364d4b6
indigoparadox <indigo.repo@interfinitdynamics.info>
2016-02-18 12:12:02 -0500

    Started conversion. Split class defs out into header and adjusted indentation.

---
+++ OpenGadget/dep/arckogako.h
@@ -0,0 +1,51 @@
+
+#ifndef ARCKOGADO_H
+#define ARCKOGADO_H
+
+
+/*
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Text;
+using GameRes.Formats.Strings;
+using GameRes.Utility;
+*/
+
+
+//namespace GameRes.Formats.Kogado
+//{
+//    public class KogadoEntry : PackedEntry
+//    {
+// 0 : Not compressed
+// 1 : Mariel compression
+// 2 : Cocotte compression
+// 3 : Xor 0xff encryption
+public byte     CompressionType;
+public bool     HasCheckSum;
+public ushort   CheckSum;
+public long     FileTime;
+//}
+
+[Export(typeof(ArchiveFormat))]
+public class PakOpener : ArchiveFormat
+{
+  public override string         Tag{ get{ return "KOGADO"; } }
+  public override string Description{ get{ return arcStrings.KogadoDescription; } }
+  public override uint     Signature{ get{ return 0x61507948; } } // 'HyPa'
+  public override bool  IsHierarchic{ get{ return false; } }
+  public override bool     CanCreate{ get{ return true; } }
+}
+
+public sealed class Crc16 : ICheckSum
+{
+  private ushort m_value = 0xffff;
+
+  public uint Value{ get{ return m_value; } }
+}
+
+#endif /* ARCKOGAKO_H */
