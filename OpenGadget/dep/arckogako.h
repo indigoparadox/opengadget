9460f53c959e08c2a84e8101075fb7f97984820a
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2016-02-18 12:25:28 -0500

    Formatting tab size to 3 spaces. Started converting first functions.

--- OpenGadget/dep/arckogako.h
+++ OpenGadget/dep/arckogako.h
@@ -16,6 +16,7 @@ using GameRes.Formats.Strings;
 using GameRes.Utility;
 */
 
+#include <stdio.h>
 
 //namespace GameRes.Formats.Kogado
 //{
@@ -25,27 +26,35 @@ using GameRes.Utility;
 // 1 : Mariel compression
 // 2 : Cocotte compression
 // 3 : Xor 0xff encryption
+/*
 public byte     CompressionType;
 public bool     HasCheckSum;
 public ushort   CheckSum;
 public long     FileTime;
+*/
 //}
 
-[Export(typeof(ArchiveFormat))]
-public class PakOpener : ArchiveFormat
-{
-  public override string         Tag{ get{ return "KOGADO"; } }
-  public override string Description{ get{ return arcStrings.KogadoDescription; } }
-  public override uint     Signature{ get{ return 0x61507948; } } // 'HyPa'
-  public override bool  IsHierarchic{ get{ return false; } }
-  public override bool     CanCreate{ get{ return true; } }
-}
+//[Export(typeof(ArchiveFormat))]
+//public class PakOpener : ArchiveFormat
+struct pak_file {
+   int test;
+//public override string         Tag{ get{ return "KOGADO"; } }
+//public override string Description{ get{ return arcStrings.KogadoDescription; } }
+//public override uint     Signature{ get{ return 0x61507948; } } // 'HyPa'
+//public override bool  IsHierarchic{ get{ return false; } }
+//public override bool     CanCreate{ get{ return true; } }
+};
 
+/*
 public sealed class Crc16 : ICheckSum
 {
-  private ushort m_value = 0xffff;
+   private ushort m_value = 0xffff;
 
-  public uint Value{ get{ return m_value; } }
+   public uint Value{ get{ return m_value; } }
 }
+*/
+
+
+void try_open( FILE*, struct pak_file* );
 
 #endif /* ARCKOGAKO_H */
