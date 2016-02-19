46946ca3d23f082e17366d2e9079133eb9b65244
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2016-02-19 09:32:57 -0500

    Added simple extraction program for testing.

---
+++ OpenGadget/extractor.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+#include "dep/arckogako.h"
+
+int main( int argc, char* argv[] ) {
+
+   struct pak_file* pak;
+   struct pak_entry* entry;
+   FILE* file;
+   FILE* file_out;
+   char* contents;
+   int i;
+   int extract;
+
+   extract = atoi( argv[2] );
+   
+   file = fopen( argv[1], "rb" );
+
+   pak = pakopener_try_open( file );
+
+   /*
+   for( i = 0 ; i < pak->count ; i++ ) {
+      printf( "%s\n", pak->entries[i].name );
+   }
+   */
+
+   entry = &(pak->entries[extract]);
+
+   contents = pakopener_open_entry( pak, file, entry );
+
+   file_out = fopen( entry->name, "wb" );
+
+   fwrite( contents, entry->unpacked_size, 1, file_out );
+
+   fclose( file_out );
+
+   return 0;
+}
