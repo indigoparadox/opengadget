461600a6fbee5f6c052ba45614146ed6713d874e
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2016-02-23 14:46:02 -0500

    Added rotation. Now the map defaults to the correct orientation but the overlap is wrong.

--- OpenGadget/tbsloop.h
+++ OpenGadget/tbsloop.h
@@ -9,6 +9,14 @@
 #include "isomap_terrain.h"
 #include "graphics.h"
 
+
+typedef enum {
+   ISORENDER_ROTATE_0,
+   ISORENDER_ROTATE_90,
+   ISORENDER_ROTATE_180,
+   ISORENDER_ROTATE_270
+} ISORENDER_ROTATE;
+
 #define tbsloop_load_texture( terrain_id ) \
    bassignformat( gfx_tile_name, gfx_tile_name_format, 1, terrain_id, 0 ); \
    terrain_images[terrain_id] = graphics_image_load( gfx_tile_name, gfx_data_pak );
