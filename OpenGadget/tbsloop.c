36c9dec2faf9d7a110ab4daa7e341c9730e9ddd9
indigoparadox <indigo.repo@interfinitdynamics.info>
2016-02-23 14:46:02 -0500

    Added rotation. Now the map defaults to the correct orientation but the overlap is wrong.

--- OpenGadget/tbsloop.c
+++ OpenGadget/tbsloop.c
@@ -5,7 +5,7 @@ extern bstring data_path;
 
 RETVAL tbsloop_loop( struct tbsloop_config* config ) {
    RETVAL retval = 0;
-   int i = 0, x, y, tile_index;
+   int i = 0, x, y, projected_x, projected_y, tile_index;
    bstring gfx_data_path = NULL;
    FILE* gfx_data_file = NULL;
    struct pak_file* gfx_data_pak = NULL;
@@ -15,6 +15,7 @@ RETVAL tbsloop_loop( struct tbsloop_config* config ) {
    SDL_Texture* sprite_texture = NULL;
    SDL_Rect viewport;
    int x_y_index;
+   ISORENDER_ROTATE rotation = ISORENDER_ROTATE_0;
 
    graphics_set_title( config->map_name );
 
@@ -52,6 +53,7 @@ RETVAL tbsloop_loop( struct tbsloop_config* config ) {
 
    viewport.w = 640;
    viewport.h = 480;
+   viewport.x = 200;
    viewport.y = 200;
 
    for( i = 0 ; 10000 > i ; i++ ) {
@@ -61,6 +63,28 @@ RETVAL tbsloop_loop( struct tbsloop_config* config ) {
 
       for( x = config->map->width - 1 ; 0 <= x ; x-- ) {
          for( y = 0 ; config->map->height > y ; y++ ) {
+
+            switch( rotation ) {
+               case ISORENDER_ROTATE_0:
+                  projected_x = y;
+                  projected_y = config->map->height - x - 1;
+                  break;
+
+               case ISORENDER_ROTATE_90:
+                  projected_x = config->map->width - x - 1;
+                  projected_y = config->map->height - y - 1;
+                  break;
+
+               case ISORENDER_ROTATE_180:
+                  projected_x = config->map->width - y - 1;
+                  projected_y = x;
+                  break;
+
+               case ISORENDER_ROTATE_270:
+                  projected_x = x;
+                  projected_y = y;
+                  break;
+            }
 
             x_y_index = (x * config->map->height) + y;
             tile_index = config->map->tiles[x_y_index];
@@ -72,8 +96,8 @@ RETVAL tbsloop_loop( struct tbsloop_config* config ) {
             graphics_draw_tile(
                sprite_texture,
                0, 0,
-               (y * GRAPHICS_TILE_WIDTH / 2) + (x * GRAPHICS_TILE_WIDTH / 2),
-               viewport.y + ((x * GRAPHICS_TILE_OFFSET_X / 2) - (y * GRAPHICS_TILE_OFFSET_X / 2))
+               viewport.x + (projected_y * GRAPHICS_TILE_WIDTH / 2) + (projected_x * GRAPHICS_TILE_WIDTH / 2),
+               viewport.y + ((projected_x * GRAPHICS_TILE_OFFSET_X / 2) - (projected_y * GRAPHICS_TILE_OFFSET_X / 2))
             );
          }
       }
