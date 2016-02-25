783d5217ade9aec4fc5836f3d46ad602da3f18ec
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2016-02-25 09:58:40 -0500

    Added rough basic unit loading support.

--- OpenGadget/isomap.h
+++ OpenGadget/isomap.h
@@ -4,23 +4,30 @@
 
 #include "defines.h"
 #include "dep/arckogako.h"
+#include "units.h"
 
-#define ISOMAP_TERRAIN_PLAINS    0x00
-#define ISOMAP_TERRAIN_ROAD      0x01
-#define ISOMAP_TERRAIN_SWAMP     0x03
-#define ISOMAP_TERRAIN_TREES     0x04
-#define ISOMAP_TERRAIN_BEACH     0x05
-#define ISOMAP_TERRAIN_SEA       0x06
-#define ISOMAP_TERRAIN_RIVER     0x07
-#define ISOMAP_TERRAIN_WASTE     0x08
-#define ISOMAP_TERRAIN_HILL      0x0A
-#define ISOMAP_TERRAIN_BRIDGE    0x0C
-#define ISOMAP_TERRAIN_HQ        0x14
-#define ISOMAP_TERRAIN_CITY      0x15
+#define ISOMAP_UNIT_DATA_SIZE 32
 
-#define ISOMAP_WEATHER_CLEAR     0x00
-#define ISOMAP_WEATHER_RAIN      0x09
-#define ISOMAP_WEATHER_SNOW      0x12
+typedef enum {
+   ISOMAP_TERRAIN_PLAINS =  0x00,
+   ISOMAP_TERRAIN_ROAD =    0x01,
+   ISOMAP_TERRAIN_SWAMP =   0x03,
+   ISOMAP_TERRAIN_TREES =   0x04,
+   ISOMAP_TERRAIN_BEACH =   0x05,
+   ISOMAP_TERRAIN_SEA =     0x06,
+   ISOMAP_TERRAIN_RIVER =   0x07,
+   ISOMAP_TERRAIN_WASTE =   0x08,
+   ISOMAP_TERRAIN_HILL =    0x0A,
+   ISOMAP_TERRAIN_BRIDGE =  0x0C,
+   ISOMAP_TERRAIN_HQ =      0x14,
+   ISOMAP_TERRAIN_CITY =    0x15
+} ISOMAP_TERRAIN;
+
+typedef enum {
+   ISOMAP_WEATHER_CLEAR =   0x00,
+   ISOMAP_WEATHER_RAIN =    0x09,
+   ISOMAP_WEATHER_SNOW =    0x12
+} ISOMAP_WEATHER;
 
 #define isomap_rendian_copy( target, cursor ) \
    for( i = 3 ; 0 <= i ; i-- ) { \
@@ -41,6 +48,7 @@ struct isomap {
    uint32_t width;
    uint32_t height;
    uint8_t weather;
+   struct units_unit* units;
    uint8_t* tiles;
 };
 
