4db2868d9e3e12b8c9733bee3b53fad13eee01bc
indigoparadox <indigo.repo@interfinitdynamics.info>
2016-02-25 09:58:40 -0500

    Added rough basic unit loading support.

--- OpenGadget/isomap.c
+++ OpenGadget/isomap.c
@@ -4,6 +4,7 @@
 struct isomap* isomap_load_map( uint8_t* map_data, uint32_t map_data_len ) {
    int32_t i;
    int32_t cursor = -1;
+   int32_t units_cursor = -1;
    uint32_t map_portion_len;
    char* data_iter = NULL;
    struct isomap* isomap_out = NULL;
@@ -23,6 +24,7 @@ struct isomap* isomap_load_map( uint8_t* map_data, uint32_t map_data_len ) {
    cursor += 4;
    isomap_rendian_copy( isomap_out->width, cursor );
 
+   /* Parse the map terrain tiles. */
    map_portion_len = isomap_out->width * isomap_out->height;
    isomap_find_section( "MAPD", map_data, map_data_len );
    cursor += 8;
@@ -37,6 +39,30 @@ struct isomap* isomap_load_map( uint8_t* map_data, uint32_t map_data_len ) {
    isomap_out->tiles = calloc( map_portion_len, sizeof( uint8_t ) );
 
    memcpy( isomap_out->tiles, &(map_data[cursor]), map_portion_len );
+
+   /* Parse the units. */
+   isomap_find_section( "MAPU", map_data, map_data_len );
+   cursor += 4;
+
+   memcpy( &map_portion_len, &(map_data[cursor]), sizeof( uint32_t ) );
+   cursor += sizeof( uint32_t );
+
+   isomap_out->units = calloc( map_portion_len / ISOMAP_UNIT_DATA_SIZE, sizeof( struct units_unit ) );
+   for( units_cursor = 0 ; units_cursor < (map_portion_len / ISOMAP_UNIT_DATA_SIZE); units_cursor ++ ) {
+      memcpy( &(isomap_out->units[units_cursor].type), &(map_data[cursor]), sizeof( uint32_t ) );
+      cursor += sizeof( uint32_t );
+
+      // ???
+      cursor += sizeof( uint32_t );
+
+      memcpy( &(isomap_out->units[units_cursor].x), &(map_data[cursor]), sizeof( uint32_t ) );
+      cursor += sizeof( uint32_t );
+
+      memcpy( &(isomap_out->units[units_cursor].y), &(map_data[cursor]), sizeof( uint32_t ) );
+      cursor += sizeof( uint32_t );
+
+      cursor += (4 * sizeof( uint32_t ));
+   }
 
 cleanup:
    
